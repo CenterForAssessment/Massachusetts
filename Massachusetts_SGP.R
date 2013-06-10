a4cca0d40f886ada2faecd874d69284477c4e6bb
dbetebenner <dbetebenner@gmail.com>
2013-06-10 11:04:53 -0400

    Update of Massachusetts_SGP

--- Massachusetts_SGP.R
+++ Massachusetts_SGP.R
@@ -17,13 +17,14 @@ load("Data/Base_Files/Massachusetts_SGP.Rdata")
 ### analyzeSGP
 
 Massachusetts_SGP <- analyzeSGP(Massachusetts_SGP,
-			sgp.percentiles=FALSE,
+			years="2012",
+			sgp.percentiles=TRUE,
 			sgp.projections=FALSE,
 			sgp.projections.lagged=FALSE,
 			sgp.percentiles.baseline=TRUE,
 			sgp.projections.baseline=TRUE,
 			sgp.projections.lagged.baseline=TRUE,
-			parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=30, BASELINE_PERCENTILES=8, PROJECTIONS=6, LAGGED_PROJECTIONS=6, SUMMARY=30, GA_PLOTS=10, SG_PLOTS=1)))
+			parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=10, BASELINE_PERCENTILES=10, PROJECTIONS=10, LAGGED_PROJECTIONS=10)))
 
 save(Massachusetts_SGP, file="Data/Massachusetts_SGP.Rdata")
 
