2f7bea5a97139a0c745ed56e43ba7fab6fd943f4
dbetebenner <dbetebenner@gmail.com>
2015-06-10 14:04:46 -0400

    Adding ELA 2015 prep file

---
+++ Massachusetts_Data_LONG_2015_ELA.R
@@ -0,0 +1,47 @@
+############################################################
+###
+### Script for prepare 2015 ELA LONG file
+###
+############################################################
+
+### Load packages
+
+require(foreign)
+require(data.table)
+require(SGP)
+
+
+### Load data
+
+Massachusetts_LONG_2015_ELA <- as.data.table(read.spss("MCAS2015_ELA.sav", to.data.frame=TRUE))
+
+
+### Change names
+
+names(Massachusetts_LONG_2015_ELA) <- c("DISTRICT_NUMBER", "SCHOOL_NUMBER", "ID", "GRADE", "TEST_STATUS", "RAW_SCORE", "SCALE_SCORE_ACTUAL", 
+	"SCALE_SCORE_LOW_SE", "SCALE_SCORE_HIGH_SE", "SCALE_SCORE", "STANDARD_ERROR")
+
+
+
+### Tidy Up Data
+
+levels(Massachusetts_LONG_2015_ELA$TEST_STATUS) <- sapply(levels(Massachusetts_LONG_2015_ELA$TEST_STATUS), capwords)
+Massachusetts_LONG_2015_ELA <- subset(Massachusetts_LONG_2015_ELA, TEST_STATUS=="T")
+Massachusetts_LONG_2015_ELA[,TEST_STATUS:=NULL]
+
+Massachusetts_LONG_2015_ELA[,RAW_SCORE:=NULL]
+Massachusetts_LONG_2015_ELA[,SCALE_SCORE_LOW_SE:=NULL]
+Massachusetts_LONG_2015_ELA[,SCALE_SCORE_HIGH_SE:=NULL]
+
+Massachusetts_LONG_2015_ELA[,SCHOOL_NUMBER := as.character(SCHOOL_NUMBER)]
+Massachusetts_LONG_2015_ELA[,DISTRICT_NUMBER:= as.character(DISTRICT_NUMBER)]
+Massachusetts_LONG_2015_ELA[,ID := as.character(ID)]
+Massachusetts_LONG_2015_ELA[,GRADE:= as.character(as.numeric(GRADE))]
+
+Massachusetts_LONG_2015_ELA[,CONTENT_AREA:="ELA"]
+Massachusetts_LONG_2015_ELA[,YEAR:="2015"]
+
+
+### Save results
+
+save(Massachusetts_LONG_2015_ELA, file="Massachusetts_Data_LONG_2015_ELA.Rdata")
