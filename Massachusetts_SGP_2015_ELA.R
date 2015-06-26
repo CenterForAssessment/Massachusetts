1fb5d23e17dbea22dd397e15f0b959f17384586d
dbetebenner <dbetebenner@gmail.com>
2015-06-26 16:36:25 -0400

    Adding 2015 SGP calculation ELA file

---
+++ Massachusetts_SGP_2015_ELA.R
@@ -0,0 +1,81 @@
+####################################################################
+###
+### Massachusetts SGP data for 2015
+###
+####################################################################
+
+### Load SGP package
+
+require(SGP)
+require(data.table)
+
+
+### Load data
+
+load("Data/Massachusetts_SGP.Rdata")
+load("Data/Massachusetts_Data_LONG_2015.Rdata")
+
+
+### Merge 2015 data with prior data
+
+Massachusetts_SGP@Data <- rbindlist(list(Massachusetts_SGP@Data, Massachusetts_Data_LONG_2015), fill=TRUE)
+
+
+### prepareSGP
+
+Massachusetts_SGP <- prepareSGP(Massachusetts_SGP)
+
+
+### STEP 1: analyzeSGP with SAMPLE to generate coefficient matrices
+
+Massachusetts_SGP <- analyzeSGP(Massachusetts_SGP,
+			years="2015",
+			content_areass="ELA",
+			sgp.percentiles=TRUE,
+			sgp.projections=FALSE,
+			sgp.projections.lagged=FALSE,
+			sgp.percentiles.baseline=FALSE,
+			sgp.projections.baseline=FALSE,
+			sgp.projections.lagged.baseline=FALSE,
+			parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=6)))
+
+save(Massachusetts_SGP, file="Data/Massachusetts_SGP.Rdata")
+
+
+### STEP 2: analyzeSGP with entire data set
+
+Massachusetts_SGP@Data$VALID_CASE[Massachusetts_SGP@Data$YEAR=="2015"] <- "VALID_CASE"
+Massachusetts_SGP@SGP$SGPercentiles$ELA.2015 <- NULL
+Massachusetts_SGP@SGP$Goodness_of_Fit$ELA.2015 <- NULL
+Massachusetts_SGP@SGP$Simulated_SGPs$ELA.2015 <- NULL
+
+Massachusetts_SGP <- prepareSGP(Massachusetts_SGP)
+
+Massachusetts_SGP <- analyzeSGP(Massachusetts_SGP,
+			years="2015",
+			content_areas="ELA",
+			sgp.percentiles=TRUE,
+			sgp.projections=FALSE,
+			sgp.projections.lagged=FALSE,
+			sgp.percentiles.baseline=FALSE,
+			sgp.projections.baseline=FALSE,
+			sgp.projections.lagged.baseline=FALSE,
+			sgp.use.my.coefficient.matrices=list(my.year="2015", my.subject="ELA"),
+			parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=6)))
+
+save(Massachusetts_SGP, file="Data/Massachusetts_SGP.Rdata")
+
+
+### combineSGP
+
+Massachusetts_SGP <- combineSGP(Massachusetts_SGP, years="2015")
+
+
+### outputSGP
+
+outputSGP(Massachusetts_SGP)
+
+
+### save results
+
+save(Massachusetts_SGP, file="Data/Massachusetts_SGP.Rdata")
