0f5a63ed9db11301de0a7fe8eb6c61cc3023aa8b
dbetebenner <dbetebenner@gmail.com>
2015-06-26 12:21:32 -0400

    Updating 2015 ELA data prep script

--- Massachusetts_Data_LONG_2015_ELA.R
+++ Massachusetts_Data_LONG_2015_ELA.R
@@ -13,33 +13,37 @@ require(SGP)
 
 ### Load data
 
-Massachusetts_LONG_2015_ELA <- as.data.table(read.spss("MCAS2015_ELA.sav", to.data.frame=TRUE))
+Massachusetts_LONG_2015_ELA <- as.data.table(read.spss("MCAS2015_ELA_7.sav", to.data.frame=TRUE, trim.factor.names = TRUE))
 
 
 ### Change names
 
-names(Massachusetts_LONG_2015_ELA) <- c("DISTRICT_NUMBER", "SCHOOL_NUMBER", "ID", "GRADE", "TEST_STATUS", "RAW_SCORE", "SCALE_SCORE_ACTUAL", 
-	"SCALE_SCORE_LOW_SE", "SCALE_SCORE_HIGH_SE", "SCALE_SCORE", "STANDARD_ERROR")
-
+setnames(Massachusetts_LONG_2015_ELA, c("ID", "GRADE", "RAW_SCORE", "TEST_STATUS", "SCALE_SCORE_ACTUAL",
+	"ACHIEVEMENT_LEVEL", "SCALE_SCORE", "STANDARD_ERROR", "VALID_CASE"))
 
 
 ### Tidy Up Data
 
-levels(Massachusetts_LONG_2015_ELA$TEST_STATUS) <- sapply(levels(Massachusetts_LONG_2015_ELA$TEST_STATUS), capwords)
+levels(Massachusetts_LONG_2015_ELA$TEST_STATUS) <- toupper(sapply(levels(Massachusetts_LONG_2015_ELA$TEST_STATUS), capwords))
 Massachusetts_LONG_2015_ELA <- subset(Massachusetts_LONG_2015_ELA, TEST_STATUS=="T")
 Massachusetts_LONG_2015_ELA[,TEST_STATUS:=NULL]
 
 Massachusetts_LONG_2015_ELA[,RAW_SCORE:=NULL]
-Massachusetts_LONG_2015_ELA[,SCALE_SCORE_LOW_SE:=NULL]
-Massachusetts_LONG_2015_ELA[,SCALE_SCORE_HIGH_SE:=NULL]
 
-Massachusetts_LONG_2015_ELA[,SCHOOL_NUMBER := as.character(SCHOOL_NUMBER)]
-Massachusetts_LONG_2015_ELA[,DISTRICT_NUMBER:= as.character(DISTRICT_NUMBER)]
+levels(Massachusetts_LONG_2015_ELA$ID) <- sapply(levels(Massachusetts_LONG_2015_ELA$ID), capwords)
 Massachusetts_LONG_2015_ELA[,ID := as.character(ID)]
-Massachusetts_LONG_2015_ELA[,GRADE:= as.character(as.numeric(GRADE))]
+levels(Massachusetts_LONG_2015_ELA$ACHIEVEMENT_LEVEL) <- sapply(levels(Massachusetts_LONG_2015_ELA$ACHIEVEMENT_LEVEL), capwords)
+Massachusetts_LONG_2015_ELA[,ACHIEVEMENT_LEVEL := as.character(ACHIEVEMENT_LEVEL)]
+
+Massachusetts_LONG_2015_ELA[,GRADE:= as.character(as.numeric(as.character(GRADE)))]
 
 Massachusetts_LONG_2015_ELA[,CONTENT_AREA:="ELA"]
 Massachusetts_LONG_2015_ELA[,YEAR:="2015"]
+
+Massachusetts_LONG_2015_ELA[,VALID_CASE:=as.character(VALID_CASE)]
+Massachusetts_LONG_2015_ELA[is.na(VALID_CASE), VALID_CASE:="INVALID_CASE"]
+Massachusetts_LONG_2015_ELA[VALID_CASE==1, VALID_CASE:="VALID_CASE"]
+
 
 
 ### Save results
