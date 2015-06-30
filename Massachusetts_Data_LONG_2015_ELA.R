720294c1acf0f03d143df35db398800303c0ffc8
dbetebenner <dbetebenner@gmail.com>
2015-06-30 15:52:04 -0400

    Tidying up names

--- Massachusetts_Data_LONG_2015_ELA.R
+++ Massachusetts_Data_LONG_2015_ELA.R
@@ -13,39 +13,39 @@ require(SGP)
 
 ### Load data
 
-Massachusetts_LONG_2015_ELA <- as.data.table(read.spss("MCAS2015_ELA_7.sav", to.data.frame=TRUE, trim.factor.names = TRUE))
+Massachusetts_Data_LONG_2015_ELA <- as.data.table(read.spss("Data/Base_Files/MCAS2015_ELA.sav", to.data.frame=TRUE, trim.factor.names = TRUE))
 
 
 ### Change names
 
-setnames(Massachusetts_LONG_2015_ELA, c("ID", "GRADE", "RAW_SCORE", "TEST_STATUS", "SCALE_SCORE_ACTUAL",
+setnames(Massachusetts_Data_LONG_2015_ELA, c("ID", "GRADE", "RAW_SCORE", "TEST_STATUS", "SCALE_SCORE_ACTUAL",
 	"ACHIEVEMENT_LEVEL", "SCALE_SCORE", "STANDARD_ERROR", "VALID_CASE"))
 
 
 ### Tidy Up Data
 
-levels(Massachusetts_LONG_2015_ELA$TEST_STATUS) <- toupper(sapply(levels(Massachusetts_LONG_2015_ELA$TEST_STATUS), capwords))
-Massachusetts_LONG_2015_ELA <- subset(Massachusetts_LONG_2015_ELA, TEST_STATUS=="T")
-Massachusetts_LONG_2015_ELA[,TEST_STATUS:=NULL]
+levels(Massachusetts_Data_LONG_2015_ELA$TEST_STATUS) <- toupper(sapply(levels(Massachusetts_Data_LONG_2015_ELA$TEST_STATUS), capwords))
+Massachusetts_Data_LONG_2015_ELA <- subset(Massachusetts_Data_LONG_2015_ELA, TEST_STATUS=="T")
+Massachusetts_Data_LONG_2015_ELA[,TEST_STATUS:=NULL]
 
-Massachusetts_LONG_2015_ELA[,RAW_SCORE:=NULL]
+Massachusetts_Data_LONG_2015_ELA[,RAW_SCORE:=NULL]
 
-levels(Massachusetts_LONG_2015_ELA$ID) <- sapply(levels(Massachusetts_LONG_2015_ELA$ID), capwords)
-Massachusetts_LONG_2015_ELA[,ID := as.character(ID)]
-levels(Massachusetts_LONG_2015_ELA$ACHIEVEMENT_LEVEL) <- sapply(levels(Massachusetts_LONG_2015_ELA$ACHIEVEMENT_LEVEL), capwords)
-Massachusetts_LONG_2015_ELA[,ACHIEVEMENT_LEVEL := as.character(ACHIEVEMENT_LEVEL)]
+levels(Massachusetts_Data_LONG_2015_ELA$ID) <- sapply(levels(Massachusetts_Data_LONG_2015_ELA$ID), capwords)
+Massachusetts_Data_LONG_2015_ELA[,ID := as.character(ID)]
+levels(Massachusetts_Data_LONG_2015_ELA$ACHIEVEMENT_LEVEL) <- sapply(levels(Massachusetts_Data_LONG_2015_ELA$ACHIEVEMENT_LEVEL), capwords)
+Massachusetts_Data_LONG_2015_ELA[,ACHIEVEMENT_LEVEL := as.character(ACHIEVEMENT_LEVEL)]
 
-Massachusetts_LONG_2015_ELA[,GRADE:= as.character(as.numeric(as.character(GRADE)))]
+Massachusetts_Data_LONG_2015_ELA[,GRADE:= as.character(as.numeric(as.character(GRADE)))]
 
-Massachusetts_LONG_2015_ELA[,CONTENT_AREA:="ELA"]
-Massachusetts_LONG_2015_ELA[,YEAR:="2015"]
+Massachusetts_Data_LONG_2015_ELA[,CONTENT_AREA:="ELA"]
+Massachusetts_Data_LONG_2015_ELA[,YEAR:="2015"]
 
-Massachusetts_LONG_2015_ELA[,VALID_CASE:=as.character(VALID_CASE)]
-Massachusetts_LONG_2015_ELA[is.na(VALID_CASE), VALID_CASE:="INVALID_CASE"]
-Massachusetts_LONG_2015_ELA[VALID_CASE==1, VALID_CASE:="VALID_CASE"]
+Massachusetts_Data_LONG_2015_ELA[,VALID_CASE:=as.character(VALID_CASE)]
+Massachusetts_Data_LONG_2015_ELA[is.na(VALID_CASE), VALID_CASE:="INVALID_CASE"]
+Massachusetts_Data_LONG_2015_ELA[VALID_CASE==1, VALID_CASE:="VALID_CASE"]
 
-
+Massachusetts_Data_LONG_2015_ELA[is.na(SCALE_SCORE), SCALE_SCORE:=-6.682260154]
 
 ### Save results
 
-save(Massachusetts_LONG_2015_ELA, file="Massachusetts_Data_LONG_2015_ELA.Rdata")
+save(Massachusetts_Data_LONG_2015_ELA, file="Data/Massachusetts_Data_LONG_2015_ELA.Rdata")
