348a29e7eeebfd4e973c79c0675f5c9170ee73b4
Adam VanIwaarden <avaniwaarden@gmail.com>
2015-10-29 09:09:53 -0600

    Run sgp.projections and student report demos

--- PARCC/SGP_CONFIG/2015/MATHEMATICS.R
+++ PARCC/SGP_CONFIG/2015/MATHEMATICS.R
@@ -6,5 +6,6 @@ MATHEMATICS_2015.config <- list(
 	ALGEBRA_I.2015 = list(
                 sgp.content.areas=c(rep('MATHEMATICS', 5), 'ALGEBRA_I'),
                 sgp.panel.years=as.character(2010:2015),
-                sgp.grade.sequences=list(c('3', '4', '5', '6', '7', '8'))))
+                sgp.grade.sequences=list(c('3', '4', '5', '6', '7', '8')),
+                sgp.projection.grade.sequences="NO_PROJECTIONS"))
 
