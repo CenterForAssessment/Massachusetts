b95fed7bfea912191643df6ece89bc83eca6d369
dbetebenner <dbetebenner@gmail.com>
2016-08-12 07:49:48 -0400

    Adding in 2015 and 2016 configurations for new Massachusetts analyses

---
+++ SGP_CONFIG/2016/ELA.R
@@ -0,0 +1,6 @@
+ELA_2016.config <- list(
+        ELA.2016 = list(
+                sgp.content.areas=rep('ELA', 6),
+                sgp.panel.years=as.character(2011:2016),
+                sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('3', '4', '5', '6'), c('3', '4', '5', '6', '7'), c('3', '4', '5', '6', '7', '8'))))
+
---
+++ SGP_CONFIG/2016/MATHEMATICS.R
@@ -0,0 +1,11 @@
+MATHEMATICS_2016.config <- list(
+        MATHEMATICS.2016 = list(
+                sgp.content.areas=rep('MATHEMATICS', 6),
+                sgp.panel.years=as.character(2011:2016),
+                sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('3', '4', '5', '6'), c('3', '4', '5', '6', '7'), c('3', '4', '5', '6', '7', '8'))),
+	ALGEBRA_I.2016 = list(
+                sgp.content.areas=c(rep('MATHEMATICS', 5), 'ALGEBRA_I'),
+                sgp.panel.years=as.character(2011:2016),
+                sgp.grade.sequences=list(c('3', '4', '5', '6', '7', '8')),
+                sgp.projection.grade.sequences="NO_PROJECTIONS"))
+
