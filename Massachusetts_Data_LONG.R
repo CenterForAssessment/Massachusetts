0f40f30b999bf535cdb9bf0e397f741d4802e50f
dbetebenner <dbetebenner@gmail.com>
2017-07-26 12:13:51 -0400

    Adding new LONG data preparation scripts

---
+++ Massachusetts_Data_LONG.R
@@ -0,0 +1,70 @@
+#####################################################################################
+###                                                                               ###
+###      Prepare and format the 2015 and 2016 Massachusetts MCAS/PARCC Data       ###
+###                                                                               ###
+#####################################################################################
+
+
+### Load libraries
+
+require(data.table)
+require(foreign)
+require(SGP)
+
+### Load data
+
+ELA_Data <- read.spss("Data/Base_Files/MCAS_ELA_Long_2017.sav", to.data.frame=TRUE, trim.factor.names=TRUE)
+MATH_Data <- read.spss("Data/Base_Files/MCAS_Math_Long_2017.sav", to.data.frame=TRUE, trim.factor.names=TRUE)
+Massachusetts_Data_LONG <- rbindlist(list(ELA_Data, MATH_Data), fill=TRUE)
+
+
+### Tidy up data
+
+setnames(Massachusetts_Data_LONG, toupper(names(Massachusetts_Data_LONG)))
+
+Massachusetts_Data_LONG[,GRADE:=as.character(as.numeric(as.character(GRADE)))]
+
+Massachusetts_Data_LONG[,ID:=as.character(ID)]
+
+setattr(Massachusetts_Data_LONG$CONTENT_AREA, "levels", c("ELA", "", "ALGEBRA_I", "MATHEMATICS"))
+Massachusetts_Data_LONG[CONTENT_AREA=="",CONTENT_AREA:=NA]
+Massachusetts_Data_LONG[,CONTENT_AREA:=as.character(CONTENT_AREA)]
+
+setattr(Massachusetts_Data_LONG$ACHIEVEMENT_LEVEL, "levels", c("", "Advanced", "Needs Improvement", "Proficient", "Warning-Failing"))
+Massachusetts_Data_LONG[,ACHIEVEMENT_LEVEL:=as.character(ACHIEVEMENT_LEVEL)]
+Massachusetts_Data_LONG[ACHIEVEMENT_LEVEL=="",ACHIEVEMENT_LEVEL:=NA]
+
+Massachusetts_Data_LONG[,YEAR:=as.character(YEAR)]
+
+setattr(Massachusetts_Data_LONG$ASSESSMENT_PROGRAM, "levels", c("", "MCAS", "PARCC"))
+Massachusetts_Data_LONG[ASSESSMENT_PROGRAM=="",ASSESSMENT_PROGRAM:=NA]
+Massachusetts_Data_LONG[,ASSESSMENT_PROGRAM:=as.factor(as.character(ASSESSMENT_PROGRAM))]
+
+setnames(Massachusetts_Data_LONG, "MODE", "TEST_MODE")
+setattr(Massachusetts_Data_LONG$TEST_MODE, "levels", c("", "Online", "Paper/Pencil"))
+Massachusetts_Data_LONG[TEST_MODE=="", TEST_MODE:=NA]
+Massachusetts_Data_LONG[,TEST_MODE:=as.factor(as.character(TEST_MODE))]
+
+setnames(Massachusetts_Data_LONG, "SCALE_SCORE_ORIGINAL", "SCALE_SCORE_NON_MODE_ADJUSTED")
+
+setnames(Massachusetts_Data_LONG, "SCALE_SCORE_STANDARD_ERROR", "SCALE_SCORE_CSEM")
+
+setnames(Massachusetts_Data_LONG, "SCALE_SCORE2", "SCALE_SCORE_ORIGINAL")
+
+Massachusetts_Data_LONG[,VALID_CASE:="VALID_CASE"]
+
+
+### Setkey and look for duplicates
+
+setkey(Massachusetts_Data_LONG, VALID_CASE, CONTENT_AREA, YEAR, GRADE, ID)
+
+### Save results
+
+save(Massachusetts_Data_LONG, file="Data/Massachusetts_Data_LONG.Rdata")
+
+
+###  Create knots and boundaries
+
+#Massachusetts_Data_LONG[YEAR <= "2016", CONTENT_AREA := paste0(CONTENT_AREA, ".2016")]
+#MA_2017_Knots_Boundaries <- createKnotsBoundaries(Massachusetts_Data_LONG)
+#save(MA_2017_Knots_Boundaries, file="MA_2017_Knots_Boundaries.Rdata")
