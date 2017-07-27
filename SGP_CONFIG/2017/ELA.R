0be8803feaf242f5524652c11743e8c7e3a3ef07
dbetebenner <dbetebenner@gmail.com>
2017-07-27 12:30:50 -0400

    Fixing up the ELA configuration

--- SGP_CONFIG/2017/ELA.R
+++ SGP_CONFIG/2017/ELA.R
@@ -4,6 +4,6 @@ ELA_2017.config <- list(
                 sgp.panel.years=as.character(2015:2017),
                 sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('4', '5', '6'), c('5', '6', '7'), c('6', '7', '8'))),
         ELA.2017 = list(
-                sgp.content.areas=rep('ELA', 2),
+                sgp.content.areas=rep('ELA', 3),
                 sgp.panel.years=as.character(c(2014, 2015, 2017)),
                 sgp.grade.sequences=list(c('7', '8', '10'))))
