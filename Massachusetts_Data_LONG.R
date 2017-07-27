46a4d685857218b0b6b70edf395c8cdad494f75a
dbetebenner <dbetebenner@gmail.com>
2017-07-27 10:52:23 -0400

    Update data prep script

--- Massachusetts_Data_LONG.R
+++ Massachusetts_Data_LONG.R
@@ -11,10 +11,12 @@ require(data.table)
 require(foreign)
 require(SGP)
 
+
 ### Load data
 
 ELA_Data <- read.spss("Data/Base_Files/MCAS_ELA_Long_2017.sav", to.data.frame=TRUE, trim.factor.names=TRUE)
 MATH_Data <- read.spss("Data/Base_Files/MCAS_Math_Long_2017.sav", to.data.frame=TRUE, trim.factor.names=TRUE)
+load("Data/Base_Files/Massachusetts_Data_LONG_2014.Rdata")
 Massachusetts_Data_LONG <- rbindlist(list(ELA_Data, MATH_Data), fill=TRUE)
 
 
@@ -49,10 +51,14 @@ setnames(Massachusetts_Data_LONG, "SCALE_SCORE_ORIGINAL", "SCALE_SCORE_NON_MODE_
 
 setnames(Massachusetts_Data_LONG, "SCALE_SCORE_STANDARD_ERROR", "SCALE_SCORE_CSEM")
 
-setnames(Massachusetts_Data_LONG, "SCALE_SCORE2", "SCALE_SCORE_ORIGINAL")
+setnames(Massachusetts_Data_LONG, "SCALE_SCORE2", "SCALE_SCORE_ACTUAL")
 
 Massachusetts_Data_LONG[,VALID_CASE:="VALID_CASE"]
 
+
+### rbind 2015, 2016, and 2017 data with 2014 data
+
+Massachusetts_Data_LONG <- rbindlist(list(Massachusetts_Data_LONG_2014, Massachusetts_Data_LONG), fill=TRUE)
 
 ### Setkey and look for duplicates
 
@@ -65,6 +71,7 @@ save(Massachusetts_Data_LONG, file="Data/Massachusetts_Data_LONG.Rdata")
 
 ###  Create knots and boundaries
 
+Massachusetts_Data_LONG[YEAR %in% c("2015", "2016"), CONTENT_AREA := paste0(CONTENT_AREA, ".2015")]
 Massachusetts_Data_LONG[YEAR=="2017", CONTENT_AREA := paste0(CONTENT_AREA, ".2017")]
 MA_2017_Knots_Boundaries <- createKnotsBoundaries(Massachusetts_Data_LONG)
 save(MA_2017_Knots_Boundaries, file="MA_2017_Knots_Boundaries.Rdata")
