99cfa85f66aa8ba57227f7804ec3d9e66d07acbc
Damian Betebenner <dbetebenner@users.noreply.github.com>
2019-05-21 10:03:15 -0400

    Update MATHEMATICS.R

--- SGP_CONFIG/2019/MATHEMATICS.R
+++ SGP_CONFIG/2019/MATHEMATICS.R
@@ -1,9 +1,9 @@
 MATHEMATICS_2019.config <- list(
         MATHEMATICS.2019 = list(
                 sgp.content.areas=rep('MATHEMATICS', 3),
-                sgp.panel.years=as.character(2016:2019),
+                sgp.panel.years=as.character(2017:2019),
                 sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('4', '5', '6'), c('5', '6', '7'), c('6', '7', '8'))),
         MATHEMATICS.2019 = list(
-                sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'MATHEMATICS'),
-                sgp.panel.years=as.character(c(2016, 2017, 2019)),
-                sgp.grade.sequences=list(c('7', '8', '10'))))
+                sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS'),
+                sgp.panel.years=as.character(c(2017, 2019)),
+                sgp.grade.sequences=list(c('8', '10'))))
