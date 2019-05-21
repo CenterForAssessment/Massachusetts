e86eab69274555cb7bf3295199bc2143e04732f9
Damian Betebenner <dbetebenner@users.noreply.github.com>
2019-05-21 10:02:33 -0400

    Update ELA.R

--- SGP_CONFIG/2019/ELA.R
+++ SGP_CONFIG/2019/ELA.R
@@ -1,9 +1,9 @@
 ELA_2019.config <- list(
         ELA.2019 = list(
                 sgp.content.areas=rep('ELA', 3),
-                sgp.panel.years=as.character(2016:2019),
+                sgp.panel.years=as.character(2017:2019),
                 sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('4', '5', '6'), c('5', '6', '7'), c('6', '7', '8'))),
         ELA.2019 = list(
-                sgp.content.areas=rep('ELA', 3),
-                sgp.panel.years=as.character(c(2015, 2016, 2019)),
-                sgp.grade.sequences=list(c('7', '8', '10'))))
+                sgp.content.areas=rep('ELA', 2),
+                sgp.panel.years=as.character(c(2017, 2019)),
+                sgp.grade.sequences=list(c('8', '10'))))
