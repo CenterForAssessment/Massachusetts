d0e6edb567536c89d408e985c010536972c90677
Damian Betebenner <dbetebenner@nciea.org>
2021-07-15 09:33:35 -0400

    Fixing path to save data

--- Massachusetts_SGP_2021_PART_A.R
+++ Massachusetts_SGP_2021_PART_A.R
@@ -44,4 +44,4 @@ Massachusetts_COVID_SGP <- updateSGP(
 )
 
 ###   Save results
-save(Massachusetts_SGP, file="Massachusetts_SGP.Rdata")
+save(Massachusetts_SGP, file="Data/Massachusetts_SGP.Rdata")
