d969f8d4dec0914aa2b1783345dbf46a19133475
Damian Betebenner <dbetebenner@nciea.org>
2021-08-17 12:52:48 -0400

    Adding in SESSION info

--- Massachusetts_Data_LONG_2021.R
+++ Massachusetts_Data_LONG_2021.R
@@ -11,13 +11,15 @@ require(foreign)
 
 
 ### Load data
-#tmp.math <- as.data.table(read.spss("Data/Base_Files/Math2021.sav", to.data.frame=TRUE, trim.factor.names=TRUE))
-#tmp.ela <- as.data.table(read.spss("Data/Base_Files/ELA2021.sav", to.data.frame=TRUE, trim.factor.names=TRUE))
+tmp.math.2021 <- as.data.table(read.spss("Data/Base_Files/Math2021_with_Session.sav", to.data.frame=TRUE, trim.factor.names=TRUE))
+tmp.ela.2021 <- as.data.table(read.spss("Data/Base_Files/ELA2021_with_Session.sav", to.data.frame=TRUE, trim.factor.names=TRUE))
 tmp.math <- as.data.table(read.spss("Data/Base_Files/Math17_21.sav", to.data.frame=TRUE, trim.factor.names=TRUE))
 tmp.ela <- as.data.table(read.spss("Data/Base_Files/ELA17_21.sav", to.data.frame=TRUE, trim.factor.names=TRUE))
 tmp.ela[,content_area:="ELA"]
+tmp.ela.2021[,content_area:="ELA"]
 
 Massachusetts_Data_LONG_2017_to_2021 <- rbindlist(list(tmp.math, tmp.ela))
+Session_Lookup <- rbindlist(list(tmp.math.2021, tmp.ela.2021))
 
 
 ### Tidy up data
@@ -45,6 +47,21 @@ Massachusetts_Data_LONG_2017_to_2021[,ACHIEVEMENT_LEVEL_ORIGINAL:=as.character(A
 
 Massachusetts_Data_LONG_2017_to_2021[,VALID_CASE:="VALID_CASE"]
 
+### Merge in 2021 SESSION Information
+setnames(Session_Lookup, toupper(names(Session_Lookup)))
+Session_Lookup <- Session_Lookup[,c("ID", "GRADE", "CONTENT_AREA", "YEAR", "SESSION"), with=FALSE]
+Session_Lookup[,GRADE:=as.character(as.numeric(GRADE))]
+Session_Lookup[,YEAR:=as.character(YEAR)]
+Session_Lookup[,SESSION:=as.integer(SESSION)]
+Session_Lookup[,VALID_CASE:="VALID_CASE"]
+
+setkey(Session_Lookup, VALID_CASE, CONTENT_AREA, YEAR, GRADE, ID)
+table(duplicated(Session_Lookup, by=key(Massachusetts_Data_LONG_2017_to_2021)))
+Session_Lookup <- Session_Lookup[!duplicated(Session_Lookup, by=key(Massachusetts_Data_LONG_2017_to_2021))]
+setkey(Session_Lookup, VALID_CASE, CONTENT_AREA, YEAR, GRADE, ID)
+setkey(Massachusetts_Data_LONG_2017_to_2021, VALID_CASE, CONTENT_AREA, YEAR, GRADE, ID)
+
+Massachusetts_Data_LONG_2017_to_2021 <- Session_Lookup[Massachusetts_Data_LONG_2017_to_2021]
 
 ### Setkey and look for duplicates
 setkey(Massachusetts_Data_LONG_2017_to_2021, VALID_CASE, CONTENT_AREA, YEAR, GRADE, ID)
@@ -52,8 +69,8 @@ table(duplicated(Massachusetts_Data_LONG_2017_to_2021, by=key(Massachusetts_Data
 
 ### Reorder columns
 
-my.order <- c("VALID_CASE", "CONTENT_AREA", "YEAR", "GRADE", "ID", "SCALE_SCORE", "ACHIEVEMENT_LEVEL", "SCALE_SCORE_STANDARD_ERROR",
-            "SCALE_SCORE_ORIGINAL", "ACHIEVEMENT_LEVEL_ORIGINAL", "SCALE_SCORE_ACTUAL", "MODE")
+my.order <- c("VALID_CASE", "CONTENT_AREA", "YEAR", "GRADE", "ID", "SCALE_SCORE", "ACHIEVEMENT_LEVEL", "SCALE_SCORE_CSEM",
+            "SCALE_SCORE_ORIGINAL", "ACHIEVEMENT_LEVEL_ORIGINAL", "SCALE_SCORE_ACTUAL", "MODE", "SESSION")
 setcolorder(Massachusetts_Data_LONG_2017_to_2021, my.order)
 
 ### Create 2021 Data
