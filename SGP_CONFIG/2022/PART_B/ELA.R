1d9c96f8c26ed2dc668949a95a1b9a1548d5b5a2
Damian Betebenner <dbetebenner@nciea.org>
2022-08-31 15:53:33 -0400

    Updating configurations and part B

---
+++ SGP_CONFIG/2022/PART_B/ELA.R
@@ -0,0 +1,6 @@
+ELA_2022.config <- list(
+        ELA.2022 = list(
+                sgp.content.areas=rep('ELA', 2),
+                sgp.panel.years=as.character(2021:2022),
+                sgp.grade.sequences=list(c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8')))
+)
---
+++ SGP_CONFIG/2022/PART_B/MATHEMATICS.R
@@ -0,0 +1,6 @@
+MATHEMATICS_2022.config <- list(
+        MATHEMATICS.2022 = list(
+                sgp.content.areas=rep('MATHEMATICS', 2),
+                sgp.panel.years=as.character(2021:2022),
+                sgp.grade.sequences=list(c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8')))
+)
