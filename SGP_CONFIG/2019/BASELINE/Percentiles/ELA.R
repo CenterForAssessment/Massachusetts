3108a142b7964582906a1a8ea252bc417e314ed5
Damian Betebenner <dbetebenner@nciea.org>
2024-10-29 10:59:55 -0400

    Adding in 10th grade SGP configurations to consecutive year analyses

--- SGP_CONFIG/2019/BASELINE/Percentiles/ELA.R
+++ SGP_CONFIG/2019/BASELINE/Percentiles/ELA.R
@@ -8,5 +8,9 @@ ELA_2019.config <- list(
 	ELA_2019 = list(
 		sgp.content.areas=rep("ELA", 3),
 		sgp.panel.years=c("2017", "2018", "2019"),
-		sgp.grade.sequences=list(c("3", "4"), c("3", "4", "5"), c("4", "5", "6"), c("5", "6", "7"), c("6", "7", "8")))
+		sgp.grade.sequences=list(c("3", "4"), c("3", "4", "5"), c("4", "5", "6"), c("5", "6", "7"), c("6", "7", "8"))),
+	ELA_2019 = list( ### INCLUDE here as SKIP YEAR 10th grade SGPs usually included with consecutive analyses
+		sgp.content.areas=rep("ELA", 3),
+		sgp.panel.years=c("2016", "2017", "2019"),
+		sgp.grade.sequences=list(c("7", "8", "10")))
 )
--- SGP_CONFIG/2019/BASELINE/Percentiles/MATHEMATICS.R
+++ SGP_CONFIG/2019/BASELINE/Percentiles/MATHEMATICS.R
@@ -8,5 +8,9 @@ MATHEMATICS_2019.config <- list(
 	MATHEMATICS_2019 = list(
 		sgp.content.areas=rep("MATHEMATICS", 3),
 		sgp.panel.years=c("2017", "2018", "2019"),
-		sgp.grade.sequences=list(c("3", "4"), c("3", "4", "5"), c("4", "5", "6"), c("5", "6", "7"), c("6", "7", "8")))
+		sgp.grade.sequences=list(c("3", "4"), c("3", "4", "5"), c("4", "5", "6"), c("5", "6", "7"), c("6", "7", "8"))),
+	MATHEMATICS_2019 = list( ### Include here as usually included in consecutive grade analyses. 
+		sgp.content.areas=rep("MATHEMATICS", 3),
+		sgp.panel.years=c("2016", "2017", "2019"),
+		sgp.grade.sequences=list(c("7", "8", "10")))
 )
